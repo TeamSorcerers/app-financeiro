d21083ae030122d0acf830be287e6c16b78e6f76
Hadaward <eduardo.gimenez07@gmail.com>
2025-09-03 15:31:08 -0400

    Atualiza configuração do ESLint, adiciona dependências e move arquivo de estilos globais para src/assets

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import baseDefault from "@phoenix35/eslint-config";
+import { dirname } from "path";
+import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname_ = dirname(filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: dirname_ });
 
 const eslintConfig = [
+  ...baseDefault,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
   {
     ignores: [
       "node_modules/**",
@@ -18,7 +21,26 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "**/dev/*",
+      "**/dist/*",
+      "**/tests/*",
+      "tsconfig.json",
     ],
+  },
+  {
+    languageOptions: { sourceType: "module" },
+    rules: {
+      // curly braces for all blocks
+      curly: [ "error", "all" ],
+      // double quotes
+      "@stylistic/js/quotes": [ "error", "double", { avoidEscape: true } ],
+      // statement beside the control
+      "@stylistic/js/nonblock-statement-body-position": [ "error", "below" ],
+      // disable camelcase rule
+      camelcase: "off",
+      // disable new-cap rule
+      "new-cap": "off",
+    },
   },
 ];
 
