af548c7f02143a9b4ca937c407cca89749dc0b47
Hadaward <eduardo.gimenez07@gmail.com>
2025-09-07 17:33:27 -0400

    feat: adiciona validação para registro de usuário com esquema de autenticação

--- src/lib/shared/constants.ts
+++ src/lib/shared/constants.ts
@@ -1,1 +1,2 @@
+export const MINIMUM_NAME_LENGTH = 2;
 export const MINIMUM_PASSWORD_LENGTH = 6;
--- src/lib/shared/schemas/auth.ts
+++ src/lib/shared/schemas/auth.ts
@@ -1,4 +1,4 @@
-import { MINIMUM_PASSWORD_LENGTH } from "@/lib/shared/constants";
+import { MINIMUM_NAME_LENGTH, MINIMUM_PASSWORD_LENGTH } from "@/lib/shared/constants";
 import z from "zod";
 
 export const AuthLoginSchema = z.object({
@@ -7,3 +7,15 @@ export const AuthLoginSchema = z.object({
 });
 
 export type AuthLoginSchemaData = z.infer<typeof AuthLoginSchema>;
+
+export const AuthRegisterSchema = z.object({
+  name: z.string().min(MINIMUM_NAME_LENGTH, "O nome deve ter pelo menos 2 caracteres"),
+  email: z.email("E-mail inválido"),
+  password: z.string().min(MINIMUM_PASSWORD_LENGTH, "A senha deve ter pelo menos 6 caracteres"),
+  confirmPassword: z.string().min(MINIMUM_PASSWORD_LENGTH, "A confirmação da senha deve ter pelo menos 6 caracteres"),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "As senhas não coincidem",
+  path: [ "confirmPassword" ], // Especifica onde a mensagem de erro deve aparecer
+});
+
+export type AuthRegisterSchemaData = z.infer<typeof AuthRegisterSchema>;
