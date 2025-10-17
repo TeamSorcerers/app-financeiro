12ffe1b17b0f9292f7c9dfada650d4fb3373858e
Lucas <lucastheodoromourofernandes@gmail.com>
2025-10-17 19:59:52 -0400

    feat: implementa rotas POST e PUT para gerenciamento de convites de grupo

---
+++ src/app/api/group/invite/route.ts
@@ -0,0 +1,9 @@
+import logger from "@/lib/server/logger";
+
+export function POST () {
+  logger.info("Recebendo convite para grupo");
+}
+
+export function PUT () {
+  logger.info("Recebendo convite para grupo");
+}
